fd297e9e1195ed8883756eaa27d1931788bf35ed
IoIIIomio <62147261+Amayakof@users.noreply.github.com>
2024-04-25 20:33:54 +0500

    front upd

--- store-app/src/app/app.component.ts
+++ store-app/src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './layout/header/header.component';
-import { FooterComponent } from './layout/footer/footer.component';
 import { CommonModule } from '@angular/common';
+import { LoginComponent } from './layout/login/login.component';
 import { HomeComponent } from './layout/home/home.component';
+import { GameDetailsComponent } from './layout/game-details/game-details.component';
+import { UserPageComponent } from './layout/user-page/user-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, HomeComponent],
+  imports: [CommonModule, RouterOutlet, LoginComponent, HomeComponent, GameDetailsComponent, UserPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
