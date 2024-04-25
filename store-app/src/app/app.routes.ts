22eacf9bdf4ece8f64726c9a8ad0e0f6cda18751
IoIIIomio <62147261+Amayakof@users.noreply.github.com>
2024-04-25 20:42:12 +0500

    upd

--- store-app/src/app/app.routes.ts
+++ store-app/src/app/app.routes.ts
@@ -1,11 +1,15 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './layout/home/home.component';
-export const routes: Routes = [
-    {
-        path: 'home',
-        component: HomeComponent,
+import { LoginComponent } from './layout/login/login.component';
+import { GameDetailsComponent } from './layout/game-details/game-details.component';
+import { UserPageComponent } from './layout/user-page/user-page.component';
+import { Page404Component } from './layout/page404/page404.component';
 
-        // path: 'pageNotFound',
-        // component: ErrorPage,
-    },
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full'},
+    { path: 'login', component: LoginComponent, title: 'Login page'},
+    { path: 'home', component: HomeComponent, title: 'Feed'},
+    { path: ':username', component: UserPageComponent, title: 'User page'},
+    { path: ':home/app-game-details', component: GameDetailsComponent, title: 'Games Details'},
+    { path: '**', component: Page404Component, title: 'ErrorPage' }
 ];
