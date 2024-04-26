7ddaa3fed01cdb49fa0dd9d4d309850c9be15919
IoIIIomio <62147261+Amayakof@users.noreply.github.com>
2024-04-26 05:57:11 +0500

    updd

--- store-app/src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { HeaderComponent } from './layout/header/header.component';
-
-export const routes: Routes = [
-    { path: '', redirectTo: 'home', pathMatch: 'full'},
-    { path: '', redirectTo: 'header', pathMatch: 'full'},
-];
